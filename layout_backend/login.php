7e7ae0299b3fee6806dba6547707de6dba248733
Anugool Tippon <anugooltippon@gmail.com>
2018-09-18 12:21:33 +0700

    Add login new

--- layout_backend/login.php
+++ layout_backend/login.php
@@ -48,7 +48,22 @@ data-open="click" data-menu="vertical-menu-modern" data-col="1-column">
                     </div>
                   </div>
                   <h6 class="card-subtitle line-on-side text-muted text-center font-small-3 pt-2">
-                    <span>Login with Modern</span>
+                    <span>Easily Using</span>
+                  </h6>
+                </div>
+                <div class="card-content">
+                  <div class="card-body pt-0 text-center">
+                    <a href="#" class="btn btn-social mb-1 mr-1 btn-outline-facebook">
+                      <span class="la la-facebook"></span>
+                      <span class="px-1">facebook</span>
+                    </a>
+                    <a href="#" class="btn btn-social mb-1 mr-1 btn-outline-google">
+                      <span class="la la-google-plus font-medium-4"></span>
+                      <span class="px-1">google</span>
+                    </a>
+                  </div>
+                  <h6 class="card-subtitle line-on-side text-muted text-center font-small-3 pt-2">
+                    <span>Login with TuengNued</span>
                   </h6>
                 </div>
                 <div class="card-content">
@@ -68,23 +83,8 @@ data-open="click" data-menu="vertical-menu-modern" data-col="1-column">
                           <i class="la la-key"></i>
                         </div>
                       </fieldset>
-                      <div class="form-group row">
-                        <div class="col-md-6 col-12 text-center text-md-left">
-                          <fieldset>
-                            <input type="checkbox" id="remember-me" class="chk-remember">
-                            <label for="remember-me"> Remember Me</label>
-                          </fieldset>
-                        </div>
-                        <div class="col-md-6 col-12 text-center text-md-right"><a href="recover-password.html" class="card-link">Forgot Password?</a></div>
-                      </div>
                       <button type="submit" class="btn btn-info btn-lg btn-block"><i class="ft-unlock"></i> Login</button>
                     </form>
-                  </div>
-                </div>
-                <div class="card-footer">
-                  <div class="">
-                    <p class="float-sm-left text-center m-0"><a href="recover-password.html" class="card-link">Recover password</a></p>
-                    <p class="float-sm-right text-center m-0">New to Moden Admin? <a href="register-simple.html" class="card-link">Sign Up</a></p>
                   </div>
                 </div>
               </div>
